34b4fac49d970d261cc171d147363904238a32af
Phai <104388483+phai-zk@users.noreply.github.com>
2024-09-02 17:49:03 +0700

    Revert "auto push"

--- COMMONCORE/libft/ft_lstadd_back_bonus.c
+++ COMMONCORE/libft/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: chinujte <chinujte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 18:39:18 by chinujte          #+#    #+#             */
-/*   Updated: 2024/09/01 18:26:46 by chinujte         ###   ########.fr       */
+/*   Updated: 2024/08/30 13:27:33 by chinujte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last_point;
+
 	if (*lst == NULL)
 	{
-		printf("%s %s HELLO\n", (char *)(*lst) -> content, (char *)new -> content);
 		*lst = new;
 		return ;
 	}
