dc1960349b870dac456a873945b5a066cb3006ef
Chisanuphong Nujtes <chinujte@z2t4c3.42bangkok.com>
2024-09-12 13:18:15 +0700

    auto push

--- COMMONCORE/PRINTF/main.c
+++ COMMONCORE/PRINTF/main.c
@@ -4,6 +4,6 @@ int	main(void)
 {
 	// printf("%p %p %p %p %p %p %p %p %p %p %p\n", -1, 1, 15, 16, 17, -__LONG_MAX__ - 1, __LONG_MAX__, -__INT_MAX__ - 1, __INT_MAX__, 0, 0);
 	// ft_printf("%p %p %p %p %p %p %p %p %p %p %p\n", -1, 1, 15, 16, 17, -__LONG_MAX__ - 1, __LONG_MAX__, -__INT_MAX__ - 1, __INT_MAX__, 0, 0);
-	printf("%s\n", 0);
-	ft_printf("%s\n", 0);
+	// printf("%d\n", "WTF");
+	ft_printf("%d\n", "WTF");
 }
